a52d6a47ff51d5b35ecb56abdede19a2006126ea
lucasvinicius <viniciaosantosdacosta37378@gmail.com>
2024-04-11 11:45:58 -0300

    a

---
+++ previso/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace previso;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ previso/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace previso;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
